0589af54e5e1b218f352fd9f72ef30f336370bc7
Drin <pavel.protskiy@yandex.ru>
2023-04-09 14:23:50 +0300

    memory usage of vectorVoid

--- libs/data_structures/vector/vectorVoid.c
+++ libs/data_structures/vector/vectorVoid.c
@@ -1,1 +1,51 @@
 #include "vectorVoid.h"
+
+void errorMemoryV() {
+    fprintf(stderr, "bad alloc");
+    exit(1);
+}
+
+vectorVoid createVectorV(size_t n, size_t baseTypeSize) {
+    int* a = (void*) malloc(baseTypeSize * n);
+
+    if (a == NULL) {
+        errorMemoryV();
+    }
+
+    vectorVoid v = {n ? a : NULL, 0, n, baseTypeSize};
+
+    return v;
+}
+
+void reserveV(vectorVoid *v, size_t newCapacity) {
+    if (newCapacity == 0) {
+        v->data = NULL;
+        v->size = 0;
+        v->capacity = 0;
+    }
+    else if (newCapacity > v->capacity) {
+        v->data = (void*) realloc(v->data, v->baseTypeSize * newCapacity);
+        v->capacity = newCapacity;
+        if (v->data == NULL) {
+            errorMemoryV();
+        }
+    }
+    else if (newCapacity < v->capacity) {
+        if (newCapacity < v->size) {
+            v->size = newCapacity;
+        }
+        v->capacity = newCapacity;
+    }
+}
+
+void clearV(vectorVoid *v) {
+    v->size = 0;
+}
+
+void shrinkToFitV(vectorVoid *v) {
+    v->data = (void*) realloc(v->data, v->baseTypeSize * v->size);
+}
+
+void deleteVectorV(vectorVoid *v) {
+    free(v->data);
+}
--- libs/data_structures/vector/vectorVoid.h
+++ libs/data_structures/vector/vectorVoid.h
@@ -2,6 +2,8 @@
 #define FORISPRITCHIN_VECTORVOID_H
 
 #include <stdlib.h>
+#include <stdio.h>
+#include <malloc.h>
 
 typedef struct vectorVoid {
     void *data;          // указатель на нулевой элемент вектора
@@ -9,5 +11,15 @@ typedef struct vectorVoid {
     size_t capacity;     // вместимость вектора
     size_t baseTypeSize; // размер базового типа:
 } vectorVoid;
+
+vectorVoid createVectorV(size_t n, size_t baseTypeSize);
+
+void reserveV(vectorVoid *v, size_t newCapacity);
+
+void shrinkToFitV(vectorVoid *v);
+
+void clearV(vectorVoid *v);
+
+void deleteVectorV(vectorVoid *v);
 
 #endif //FORISPRITCHIN_VECTORVOID_H
